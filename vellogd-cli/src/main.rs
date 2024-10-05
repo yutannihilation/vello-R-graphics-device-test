950236f43375b22261917d1b4644b81f5c25942b
Hiroaki Yutani <yutani.ini@gmail.com>
2024-10-05 21:05:24 +0900

    Migrate to clap

--- vellogd-cli/src/main.rs
+++ vellogd-cli/src/main.rs
@@ -1,9 +1,6 @@
 use vellogd_protocol::graphics_device_client::GraphicsDeviceClient;
 use vellogd_protocol::*;
 
-const MEDIUM_PURPLE: u32 = u32::from_ne_bytes([147, 112, 219, 255]);
-const PALE_GREEN: u32 = u32::from_ne_bytes([152, 251, 152, 255]);
-
 use clap::{Parser, Subcommand};
 
 fn hex_color_to_u32<T: AsRef<str>>(x: T) -> u32 {
@@ -41,6 +38,9 @@ enum Commands {
     Close {},
 
     #[command()]
+    Clear {},
+
+    #[command()]
     Circle {
         #[arg()]
         cx: f64,
@@ -55,6 +55,68 @@ enum Commands {
         #[arg(long, short, default_value = "000")]
         color: String,
     },
+
+    #[command()]
+    Line {
+        #[arg()]
+        x0: f64,
+        #[arg()]
+        y0: f64,
+        #[arg()]
+        x1: f64,
+        #[arg()]
+        y1: f64,
+        #[arg(long, short, default_value_t = 8.0)]
+        width: f64,
+        #[arg(long, short, default_value = "000")]
+        color: String,
+    },
+
+    #[command()]
+    Lines {
+        #[arg()]
+        pos: Vec<f64>,
+        #[arg(long, short, default_value_t = 8.0)]
+        width: f64,
+        #[arg(long, short, default_value = "000")]
+        color: String,
+    },
+
+    #[command()]
+    Polygon {
+        #[arg()]
+        pos: Vec<f64>,
+        #[arg(long, short, default_value_t = 8.0)]
+        width: f64,
+        #[arg(long, short, default_value = "999")]
+        fill: String,
+        #[arg(long, short, default_value = "000")]
+        color: String,
+    },
+
+    #[command()]
+    Text {
+        #[arg()]
+        x: f64,
+        #[arg()]
+        y: f64,
+        #[arg(default_value = "🌶")] // to test emoji (cannot input from Powershell)
+        text: String,
+        #[arg(long, short, default_value = "000")]
+        color: String,
+        #[arg(long, short, default_value_t = 50.0)]
+        size: f32,
+        #[arg(long, default_value_t = 1.0)]
+        lineheight: f32,
+        #[arg(long, default_value_t = 1)]
+        face: u32,
+        #[arg(long, default_value = "Arial")]
+        family: String,
+        #[arg(long, default_value_t = 0.0)]
+        angle: f32,
+        #[arg(long, default_value_t = 0.0)]
+        hadj: f32,
+    },
 }
 
 #[tokio::main]
@@ -65,10 +127,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut client = GraphicsDeviceClient::connect("http://[::1]:50051").await?;
 
     let response = match args.command {
-        Commands::Close {} => {
-            let request = tonic::Request::new(Empty {});
-            client.close_window(request).await
-        }
+        Commands::Close {} => client.close_window(Empty {}).await,
+        Commands::Clear {} => client.new_page(Empty {}).await,
 
         Commands::Circle {
             cx,
@@ -79,57 +139,56 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             color,
         } => {
             let fill_color = hex_color_to_u32(fill);
+            let fill_color = if fill_color != 0 {
+                Some(fill_color)
+            } else {
+                None
+            };
+
             let stroke_color = hex_color_to_u32(color);
-            let stroke_params = StrokeParameters {
-                color: stroke_color,
-                width,
-                linetype: 1,
-                join: 1,
-                miter_limit: 1.0,
-                cap: 1,
+            let stroke_params = if stroke_color != 0 {
+                Some(StrokeParameters {
+                    color: stroke_color,
+                    width,
+                    linetype: 1,
+                    join: 1,
+                    miter_limit: 1.0,
+                    cap: 1,
+                })
+            } else {
+                None
             };
 
             let request = tonic::Request::new(DrawCircleRequest {
                 cx,
                 cy,
                 radius,
-                fill_color: Some(fill_color),
-                stroke_params: Some(stroke_params),
+                fill_color,
+                stroke_params,
             });
             client.draw_circle(request).await
         }
-    }?;
 
-    println!("RESPONSE={:?}", response);
-
-    return Ok(());
-
-    let subcommand = std::env::args().nth(1).unwrap_or_default();
-    println!("{subcommand}");
-    let response_ = match subcommand.as_str() {
-        "line" => {
-            let x0: f64 = std::env::args()
-                .nth(2)
-                .unwrap_or_default()
-                .parse()
-                .unwrap_or(100.0);
-            let y0: f64 = std::env::args()
-                .nth(3)
-                .unwrap_or_default()
-                .parse()
-                .unwrap_or(100.0);
-            let x1 = x0 + 100.0;
-            let y1 = y0 + 100.0;
-
-            let color = MEDIUM_PURPLE;
-            let width = 15.0;
-            let stroke_params = StrokeParameters {
-                color,
-                width,
-                linetype: 1,
-                join: 1,
-                miter_limit: 1.0,
-                cap: 1,
+        Commands::Line {
+            x0,
+            y0,
+            x1,
+            y1,
+            width,
+            color,
+        } => {
+            let color = hex_color_to_u32(color);
+            let stroke_params = if color != 0 {
+                Some(StrokeParameters {
+                    color,
+                    width,
+                    linetype: 1,
+                    join: 1,
+                    miter_limit: 1.0,
+                    cap: 1,
+                })
+            } else {
+                None
             };
 
             let request = tonic::Request::new(DrawLineRequest {
@@ -137,77 +196,121 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 y0,
                 x1,
                 y1,
-                stroke_params: Some(stroke_params),
+                stroke_params,
             });
             client.draw_line(request).await
         }
-        "lines" => {
-            let color = MEDIUM_PURPLE;
-            let width = 15.0;
-            let stroke_params = StrokeParameters {
-                color,
-                width,
-                linetype: 1,
-                join: 1,
-                miter_limit: 1.0,
-                cap: 1,
+
+        Commands::Lines { pos, width, color } => {
+            if pos.len() < 4 || pos.len() % 2 != 0 {
+                panic!("invalid number of arguments; the locations need to be pairs of X and Y");
+            }
+
+            let mut x = Vec::new();
+            let mut y = Vec::new();
+
+            for p in pos.chunks_exact(2) {
+                x.push(p[0]);
+                y.push(p[1]);
+            }
+
+            let color = hex_color_to_u32(color);
+            let stroke_params = if color != 0 {
+                Some(StrokeParameters {
+                    color,
+                    width,
+                    linetype: 1,
+                    join: 1,
+                    miter_limit: 1.0,
+                    cap: 1,
+                })
+            } else {
+                None
             };
 
             let request = tonic::Request::new(DrawPolylineRequest {
-                x: vec![100.0, 300.0, 500.0],
-                y: vec![100.0, 500.0, 300.0],
-                stroke_params: Some(stroke_params),
+                x,
+                y,
+                stroke_params,
             });
             client.draw_polyline(request).await
         }
-        "polygon" => {
-            let color = MEDIUM_PURPLE;
-            let width = 15.0;
-            let stroke_params = StrokeParameters {
-                color,
-                width,
-                linetype: 1,
-                join: 1,
-                miter_limit: 1.0,
-                cap: 1,
+
+        Commands::Polygon {
+            pos,
+            width,
+            fill,
+            color,
+        } => {
+            if pos.len() < 6 || pos.len() % 2 != 0 {
+                panic!("invalid number of arguments; the locations need to be pairs of X and Y");
+            }
+
+            let mut x = Vec::new();
+            let mut y = Vec::new();
+
+            for p in pos.chunks_exact(2) {
+                x.push(p[0]);
+                y.push(p[1]);
+            }
+
+            let fill_color = hex_color_to_u32(fill);
+            let fill_color = if fill_color != 0 {
+                Some(fill_color)
+            } else {
+                None
             };
+
+            let color = hex_color_to_u32(color);
+            let stroke_params = if color != 0 {
+                Some(StrokeParameters {
+                    color,
+                    width,
+                    linetype: 1,
+                    join: 1,
+                    miter_limit: 1.0,
+                    cap: 1,
+                })
+            } else {
+                None
+            };
+
             let request = tonic::Request::new(DrawPolygonRequest {
-                x: vec![100.0, 300.0, 500.0],
-                y: vec![100.0, 500.0, 300.0],
-                fill_color: Some(PALE_GREEN),
-                stroke_params: Some(stroke_params),
+                x,
+                y,
+                fill_color,
+                stroke_params,
             });
             client.draw_polygon(request).await
         }
-        "text" => {
-            let x: f64 = std::env::args()
-                .nth(2)
-                .unwrap_or_default()
-                .parse()
-                .unwrap_or(100.0);
-            let y: f64 = std::env::args()
-                .nth(3)
-                .unwrap_or_default()
-                .parse()
-                .unwrap_or(100.0);
-            let text = std::env::args().nth(4).unwrap_or("🥷".into());
 
-            let color = u32::from_ne_bytes([147, 112, 219, 255]);
+        Commands::Text {
+            x,
+            y,
+            text,
+            color,
+            size,
+            lineheight,
+            face,
+            family,
+            angle,
+            hadj,
+        } => {
+            let color = hex_color_to_u32(color);
             let request = tonic::Request::new(DrawTextRequest {
                 x,
                 y,
                 text,
                 color,
-                size: 100.0,
-                lineheight: 1.2,
-                face: 1,
-                family: "Arial".into(),
-                angle: 30.0_f32.to_radians(),
-                hadj: 0.0,
+                size,
+                lineheight,
+                face,
+                family,
+                angle,
+                hadj,
             });
             client.draw_text(request).await
         }
-        _ => client.new_page(Empty {}).await,
     }?;
 
     println!("RESPONSE={:?}", response);
